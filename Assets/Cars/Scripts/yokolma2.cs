65a27ab0353f1274ec9fc1bcdc8cd70516c35649
Gokberk Hıdıroğlu <gokbey5801@gmail.com>
2018-06-03 02:59:52 +0300

    Son düzenleme-Commit3

---
+++ Assets/Cars/Scripts/yokolma2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yokolma2 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(90 * Time.deltaTime, 0, 0);
+    }
+    private void OnTriggerEnter(Collider ad)
+    {
+        if (ad.name == "FireGTO")
+        {
+            Destroy(gameObject);
+            skor2.puan--;
+
+
+
+        }
+    }
+}
