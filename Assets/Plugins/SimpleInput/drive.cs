e22a816c5cad2c409a6b48826a3148f13806bf50
Gokberk Hıdıroğlu <gokbey5801@gmail.com>
2018-11-13 01:08:00 +0300

    android uyum
    direksiyon gaz fren ekleme

---
+++ Assets/Plugins/SimpleInput/drive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drive : MonoBehaviour {
+
+    private float inputHor;
+    private float inputver;
+    private string horizontalAxis = "Horizontal";
+    private string verticalAxis = "Vertical";
+
+	// Update is called once per frame
+	void Update () {
+        
+        inputHor = SimpleInput.GetAxis(horizontalAxis);
+        inputver = SimpleInput.GetAxis(verticalAxis);
+        transform.Rotate(0, inputHor * 5f, 0f, Space.Self);
+		
+	}
+}
